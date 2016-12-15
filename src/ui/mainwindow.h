5ade50bf9d6bc0369cc0dda2f8dae291f1075832
bleikur5 <eikibleiki55@gmail.com>
2016-12-15 14:21:44 +0000

    SearchFilter
    Implementing search stuff

--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -9,6 +9,8 @@
 #include "services/computerservice.h"
 #include "services/scientistservice.h"
 
+using namespace std;
+
 namespace Ui {
 class MainWindow;
 }
@@ -26,10 +28,17 @@ private slots:
 
 
 
+    void on_SearchScientist_textEdited(const QString &arg1);
+
+
+
 private:
     Ui::MainWindow *ui;
     ScientistService _scientistService;
     ComputerService _computerService;
+
+    std::vector<Scientist> currentScientist;
+    std::vector<Scientist> currentlyDisplayedScientist;
 
     void displayScientistTable(QString filter = "");
     void displayComputerTable();
