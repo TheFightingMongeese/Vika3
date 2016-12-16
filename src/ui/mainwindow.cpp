8ae11958decba69ce951c45e493200657afea252
Sigga <sigga@hotmail.com>
2016-12-16 15:00:48 +0000

    lagar build

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -2,7 +2,7 @@
 #include "ui_mainwindow.h"
 #include "numberwidgetitem.h"
 #include "utilities/utils.h"
-
+#include <iostream>
 #include <string>
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -22,7 +22,6 @@ MainWindow::~MainWindow()
 void MainWindow::displayScientistTable(QString filter)
 {
     std::vector<Scientist> scientists;
-
     if(filter.isEmpty())
     {
         scientists = _scientistService.getAllScientists("name", true);
@@ -105,51 +104,7 @@ void MainWindow::displayComputerTable(QString filter)
         }
      }
 }
-/*                                                                     Reyna útfæra display relations hérna
-void MainWindow::displayScientistTable(QString filter)
-{
-    std::vector<Scientist> scientists;
 
-    if(filter.isEmpty())
-    {
-        scientists = _scientistService.getAllScientists("name", true);
-    }
-    else
-    {
-        scientists = _scientistService.searchForScientists(filter.toStdString());
-    }
-
-    QTableWidget *table = ui->tableWidgetScientists;
-    table->clear();
-
-    QStringList tableHeader;
-    tableHeader << "ID" << "Name" << "Gender" << "Year born" << "Year of death";
-    table->setColumnCount(tableHeader.size());
-    table->setRowCount(scientists.size());
-    table->setHorizontalHeaderLabels(tableHeader);
-
-    if(scientists.size() > 0)
-    {
-        for(unsigned int i = 0; i < scientists.size(); i++)
-        {
-            Scientist s = scientists.at(i);
-            QString scientistID = QString::number(s.getId());
-            QString name = QString::fromStdString(s.getName());
-            QString sexType = QString::fromStdString(utils::sexToString(s.getSex()));
-            QString yearBorn = QString::number(s.getYearBorn());
-            QString yearDied = QString::number(s.getYearDied());
-
-
-            qDebug() << scientistID << name;
-
-            table->setItem(i, 0, new NumberWidgetItem(scientistID));
-            table->setItem(i, 1, new QTableWidgetItem(name));
-            table->setItem(i, 2, new QTableWidgetItem(sexType));
-            table->setItem(i, 3, new QTableWidgetItem(yearBorn));
-            table->setItem(i, 4, new QTableWidgetItem(yearDied));
-        }
-    }
-}*/
 void MainWindow::on_Tabs_currentChanged(int index)
 {
     switch(index)
@@ -160,10 +115,6 @@ void MainWindow::on_Tabs_currentChanged(int index)
     case 1:
         displayComputerTable();
         break;
-   /* case 2:                                // bætti við display relations table, á eftir að útfæra
-        displayRelationsTable();
-        break;*/
-
     }
 
 }
@@ -241,15 +192,3 @@ void MainWindow::on_btnAddComputer_clicked()
     }
 }
 
-void MainWindow::on_SearchRelations_textEdited(const QString &arg1)
-{
-
-}
-
-void MainWindow::on_BtnAddRelations_clicked()
-{
-
-
-
-
-}
